dbc99b85f0ee88a5584498d5a9a2538fcc86c451
Ilya Shumilin <shumilin@skbkontur.ru>
2021-10-19 11:38:02 +0500

    Add initial models

---
+++ Vostok.ServiceDiscovery.Telemetry/EventSender/IServiceDiscoveryEventSender.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+using Vostok.ServiceDiscovery.Telemetry.Event;
+
+namespace Vostok.ServiceDiscovery.Telemetry.EventSender
+{
+    [PublicAPI]
+    public interface IServiceDiscoveryEventSender
+    {
+        void Send([NotNull] ServiceDiscoveryEvent serviceDiscoveryEvent);
+    }
+}
