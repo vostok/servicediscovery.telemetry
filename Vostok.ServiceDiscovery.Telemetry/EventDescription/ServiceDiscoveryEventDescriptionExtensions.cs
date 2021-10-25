71b542ba4206b5c449f4989d10ad983c9dccfad1
Ilya Shumilin <shumilin@skbkontur.ru>
2021-10-25 14:51:02 +0500

    Add service discovery event description

---
+++ Vostok.ServiceDiscovery.Telemetry/EventDescription/ServiceDiscoveryEventDescriptionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+
+namespace Vostok.ServiceDiscovery.Telemetry.EventDescription
+{
+    [PublicAPI]
+    public static class ServiceDiscoveryEventDescriptionExtensions
+    {
+        [NotNull]
+        public static ServiceDiscoveryEventDescription SetDescription(this ServiceDiscoveryEventDescription serviceDiscoveryEventsBuilder, [NotNull] string description) =>
+            serviceDiscoveryEventsBuilder.AddProperties((ServiceDiscoveryEventKeys.EventDescription, description));
+        
+        [NotNull]
+        public static ServiceDiscoveryEventDescription SetUserId(this ServiceDiscoveryEventDescription serviceDiscoveryEventsBuilder, [NotNull] string userId) =>
+            serviceDiscoveryEventsBuilder.AddProperties((ServiceDiscoveryEventKeys.EventCreatorId, userId));
+    }
+}
