4d426ef9b188ae2ec3bd6aaccc2135244f3b9ef7
Ilya Shumilin <shumilin@skbkontur.ru>
2021-11-22 15:42:43 +0500

    Rename description extensions

---
+++ Vostok.ServiceDiscovery.Telemetry/Extensions/IServiceDiscoveryEventDescriptionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+using Vostok.ServiceDiscovery.Telemetry.EventDescription;
+
+namespace Vostok.ServiceDiscovery.Telemetry.Extensions
+{
+    [PublicAPI]
+    public static class IServiceDiscoveryEventDescriptionExtensions
+    {
+        [NotNull]
+        public static IServiceDiscoveryEventDescription SetDescription(this IServiceDiscoveryEventDescription serviceDiscoveryEventDescription, [NotNull] string description) =>
+            serviceDiscoveryEventDescription.AddProperty(ServiceDiscoveryEventKeys.Description, description);
+
+        [NotNull]
+        public static IServiceDiscoveryEventDescription SetUserId(this IServiceDiscoveryEventDescription serviceDiscoveryEventDescription, [NotNull] string userId) =>
+            serviceDiscoveryEventDescription.AddProperty(ServiceDiscoveryEventKeys.CreatorId, userId);
+
+        [NotNull]
+        public static IServiceDiscoveryEventDescription SetDependencies(this IServiceDiscoveryEventDescription serviceDiscoveryEventDescription, [NotNull] string dependencies) =>
+            serviceDiscoveryEventDescription.AddProperty(ServiceDiscoveryEventKeys.Dependencies, dependencies);
+    }
+}
