8347246da0351a3b1567fd717a8ecc08f58c292e
Ilya Shumilin <shumilin@skbkontur.ru>
2021-12-03 22:49:08 +0500

    Add service discovery event context token

---
+++ Vostok.ServiceDiscovery.Telemetry/EventContext/ServiceDiscoveryEventContextToken.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using JetBrains.Annotations;
+using Vostok.Context;
+using Vostok.ServiceDiscovery.Telemetry.EventsBuilder;
+
+namespace Vostok.ServiceDiscovery.Telemetry.EventContext
+{
+    [PublicAPI]
+    public class ServiceDiscoveryEventContextToken : IDisposable
+    {
+        private readonly IDisposable scope;
+
+        public ServiceDiscoveryEventContextToken([NotNull] Action<IServiceDiscoveryEventsBuilder> setup)
+        {
+            var newBuilder = FlowingContext.Globals.Get<ServiceDiscoveryEventsBuilder>()?.Clone() ?? new ServiceDiscoveryEventsBuilder();
+            setup.Invoke(newBuilder);
+
+            scope = FlowingContext.Globals.Use(newBuilder);
+        }
+
+        public void Dispose()
+        {
+            scope?.Dispose();
+        }
+    }
+}
