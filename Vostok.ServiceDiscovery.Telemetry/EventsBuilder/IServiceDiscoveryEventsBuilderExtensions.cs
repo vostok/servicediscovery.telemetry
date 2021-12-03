c81311bb02ecea14ca67a39cc574ceff3286719e
Ilya Shumilin <shumilin@skbkontur.ru>
2021-12-03 22:38:46 +0500

    Move extensions to model folder

---
+++ Vostok.ServiceDiscovery.Telemetry/EventsBuilder/IServiceDiscoveryEventsBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+using Vostok.ServiceDiscovery.Telemetry.Event;
+
+namespace Vostok.ServiceDiscovery.Telemetry.EventsBuilder
+{
+    [PublicAPI]
+    public static class IServiceDiscoveryEventsBuilderExtensions
+    {
+        [NotNull]
+        public static IServiceDiscoveryEventsBuilder SetDescription(this IServiceDiscoveryEventsBuilder serviceDiscoveryEventsBuilder, [NotNull] string description) =>
+            serviceDiscoveryEventsBuilder.SetProperty(ServiceDiscoveryWellKnownProperties.Description, description);
+
+        [NotNull]
+        public static IServiceDiscoveryEventsBuilder SetUserId(this IServiceDiscoveryEventsBuilder serviceDiscoveryEventsBuilder, [NotNull] string userId) =>
+            serviceDiscoveryEventsBuilder.SetProperty(ServiceDiscoveryWellKnownProperties.UserId, userId);
+
+        [NotNull]
+        public static IServiceDiscoveryEventsBuilder SetDependencies(this IServiceDiscoveryEventsBuilder serviceDiscoveryEventsBuilder, [NotNull] string dependencies) =>
+            serviceDiscoveryEventsBuilder.SetProperty(ServiceDiscoveryWellKnownProperties.Dependencies, dependencies);
+    }
+}
