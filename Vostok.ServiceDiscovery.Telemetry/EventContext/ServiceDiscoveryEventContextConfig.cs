67a8f4335162148056a9c98629fbd0085f739563
Ilya Shumilin <shumilin@skbkontur.ru>
2021-12-03 22:48:36 +0500

    Add service discovery event context

---
+++ Vostok.ServiceDiscovery.Telemetry/EventContext/ServiceDiscoveryEventContextConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using JetBrains.Annotations;
+using Vostok.ServiceDiscovery.Telemetry.EventSender;
+
+namespace Vostok.ServiceDiscovery.Telemetry.EventContext
+{
+    [PublicAPI]
+    public class ServiceDiscoveryEventContextConfig
+    {
+        public ServiceDiscoveryEventContextConfig([NotNull] IServiceDiscoveryEventSender sender)
+            => Sender = sender ?? throw new ArgumentNullException(nameof(sender));
+        
+        [NotNull]
+        public IServiceDiscoveryEventSender Sender { get; }
+    }
+}
