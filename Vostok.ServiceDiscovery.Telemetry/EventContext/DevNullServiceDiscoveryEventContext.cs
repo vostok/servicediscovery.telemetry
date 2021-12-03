246bb562eda54f30c07e5eb7c67d3a86c6e5af40
Ilya Shumilin <shumilin@skbkontur.ru>
2021-12-03 22:48:36 +0500

    Add service discovery event context

---
+++ Vostok.ServiceDiscovery.Telemetry/EventContext/DevNullServiceDiscoveryEventContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using Vostok.ServiceDiscovery.Telemetry.Event;
+
+namespace Vostok.ServiceDiscovery.Telemetry.EventContext
+{
+    [PublicAPI]
+    public class DevNullServiceDiscoveryEventContext : IServiceDiscoveryEventContext
+    {
+        public IReadOnlyList<ServiceDiscoveryEvent> CurrentEvents { get; } = new List<ServiceDiscoveryEvent>();
+
+        public void Send(ServiceDiscoveryEvent @event)
+        {
+        }
+
+        public bool TrySendFromContext() => true;
+    }
+}
---
+++ Vostok.ServiceDiscovery.Telemetry/EventContext/IServiceDiscoveryEventContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using Vostok.ServiceDiscovery.Telemetry.Event;
+
+namespace Vostok.ServiceDiscovery.Telemetry.EventContext
+{
+    [PublicAPI]
+    public interface IServiceDiscoveryEventContext
+    {
+        [NotNull]
+        IReadOnlyList<ServiceDiscoveryEvent> CurrentEvents { get; }
+        
+        void Send(ServiceDiscoveryEvent @event);
+        bool TrySendFromContext();
+    }
+}
