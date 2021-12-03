bf8377ea169c41ca0aeb0696c1a40af33d3d9dc4
Ilya Shumilin <shumilin@skbkontur.ru>
2021-12-03 22:38:46 +0500

    Move extensions to model folder

---
+++ Vostok.ServiceDiscovery.Telemetry/EventSender/IServiceDiscoveryEventSenderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using Vostok.ServiceDiscovery.Telemetry.Event;
+
+namespace Vostok.ServiceDiscovery.Telemetry.EventSender
+{
+    [PublicAPI]
+    public static class IServiceDiscoveryEventSenderExtensions
+    {
+        public static void Send(this IServiceDiscoveryEventSender eventSender, [NotNull] IEnumerable<ServiceDiscoveryEvent> events)
+        {
+            foreach (var @event in events)
+                eventSender.Send(@event);
+        }
+    }
+}
