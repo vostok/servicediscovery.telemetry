8d8b7114e6bd9d66154230ef95ebc0b5734c8a8f
Ilya Shumilin <shumilin@skbkontur.ru>
2021-12-03 22:42:34 +0500

    Add service discovery events builder

---
+++ Vostok.ServiceDiscovery.Telemetry/EventsBuilder/IServiceDiscoveryEventsBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using JetBrains.Annotations;
+using Vostok.ServiceDiscovery.Telemetry.Event;
+
+namespace Vostok.ServiceDiscovery.Telemetry.EventsBuilder
+{
+    [PublicAPI]
+    public interface IServiceDiscoveryEventsBuilder
+    {
+        [NotNull]
+        IServiceDiscoveryEventsBuilder SetApplication([NotNull] string application);
+
+        [NotNull]
+        IServiceDiscoveryEventsBuilder SetEnvironment([NotNull] string environment);
+
+        [NotNull]
+        IServiceDiscoveryEventsBuilder SetEventKind(ServiceDiscoveryEventKind eventKind);
+
+        [NotNull]
+        IServiceDiscoveryEventsBuilder SetProperty([NotNull] string key, [NotNull] string value);
+
+        [NotNull]
+        IServiceDiscoveryEventsBuilder SetTimestamp(DateTimeOffset timestamp);
+
+        [NotNull]
+        IServiceDiscoveryEventsBuilder AddReplicas(params string[] replicas);
+    }
+}
