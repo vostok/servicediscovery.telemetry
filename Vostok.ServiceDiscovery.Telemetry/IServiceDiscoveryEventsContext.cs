fc9659195eaf7b8278f784a8ac88b2b4908bbaa7
Ilya Shumilin <shumilin@skbkontur.ru>
2021-12-06 17:44:32 +0500

    Refactor main implementations

--- Vostok.ServiceDiscovery.Telemetry/IServiceDiscoveryEventsContext.cs
+++ Vostok.ServiceDiscovery.Telemetry/IServiceDiscoveryEventsContext.cs
@@ -3,31 +3,31 @@ using JetBrains.Annotations;
 using Vostok.ServiceDiscovery.Telemetry.Event;
 using Vostok.ServiceDiscovery.Telemetry.EventsBuilder;
 
-namespace Vostok.ServiceDiscovery.Telemetry.EventsContext
+namespace Vostok.ServiceDiscovery.Telemetry
 {
     /// <summary>
-    /// <see cref="IServiceDiscoveryEventContext"/> is the main point of sending servicediscovery telemetry.
-    /// See <see cref="CurrentEvents"/>, <see cref="Send"/>, <see cref="TrySendFromContext"/> for details. 
+    /// <see cref="IServiceDiscoveryEventsContext"/> is the main point of sending servicediscovery telemetry.
+    /// See <see cref="CurrentEvents"/>, <see cref="Send"/>, <see cref="SendFromContext"/> for details. 
     /// </summary>
     [PublicAPI]
     public interface IServiceDiscoveryEventsContext
     {
         /// <summary>
         /// <para>Returns current state of the constructed events.</para>
-        /// <para>Based on the current <see cref="IServiceDiscoveryEventsBuilder"/> configuration produced in the <see cref="ServiceDiscoveryEventContextToken"/>.</para>
+        /// <para>Based on the current <see cref="IServiceDiscoveryEventsBuilder"/> configuration produced in the <see cref="ServiceDiscoveryEventsContextToken"/>.</para>
         /// <para>Empty if environment, application and replicas is not set.</para>
         /// </summary>
         [NotNull]
         IEnumerable<ServiceDiscoveryEvent> CurrentEvents { get; }
         
         /// <summary>
-        /// Sends ready-made events using <see cref="ServiceDiscoveryEventContextConfig.Sender"/>.
+        /// Sends ready-made events using <see cref="ServiceDiscoveryEventsContextConfig.Sender"/>.
         /// </summary>
         void Send(ServiceDiscoveryEvent @event);
         
         /// <summary>
-        /// Sends events from <see cref="CurrentEvents"/> using <see cref="ServiceDiscoveryEventContextConfig.Sender"/>.
+        /// Sends events from <see cref="CurrentEvents"/> using <see cref="ServiceDiscoveryEventsContextConfig.Sender"/>.
         /// </summary>
-        bool TrySendFromContext();
+        void SendFromContext();
     }
 }
---
+++ Vostok.ServiceDiscovery.Telemetry/ServiceDiscoveryEventsContextConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using JetBrains.Annotations;
+using Vostok.ServiceDiscovery.Telemetry.EventsSender;
+
+namespace Vostok.ServiceDiscovery.Telemetry
+{
+    /// <summary>
+    /// Configuration governing behaviour of <see cref="ServiceDiscoveryEventsContext"/>.
+    /// </summary>
+    [PublicAPI]
+    public class ServiceDiscoveryEventsContextConfig
+    {
+        public ServiceDiscoveryEventsContextConfig([NotNull] IServiceDiscoveryEventsSender sender)
+            => Sender = sender ?? throw new ArgumentNullException(nameof(sender));
+
+        /// <summary>
+        /// <para>Gets the sender used to sending constructed events. See <see cref="IServiceDiscoveryEventsSender"/> for more details.</para>
+        /// <para>This configuration parameter is <b>required</b>: <c>null</c> values are not allowed.</para>
+        /// </summary>
+        [NotNull]
+        public IServiceDiscoveryEventsSender Sender { get; }
+    }
+}
