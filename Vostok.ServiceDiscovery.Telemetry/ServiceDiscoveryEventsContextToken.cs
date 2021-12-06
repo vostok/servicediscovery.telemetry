66d5ef6496e4f3473111a36c8d9501c368894395
Ilya Shumilin <shumilin@skbkontur.ru>
2021-12-06 17:44:32 +0500

    Refactor main implementations

--- Vostok.ServiceDiscovery.Telemetry/ServiceDiscoveryEventsContextToken.cs
+++ Vostok.ServiceDiscovery.Telemetry/ServiceDiscoveryEventsContextToken.cs
@@ -3,10 +3,10 @@ using JetBrains.Annotations;
 using Vostok.Context;
 using Vostok.ServiceDiscovery.Telemetry.EventsBuilder;
 
-namespace Vostok.ServiceDiscovery.Telemetry.EventContext
+namespace Vostok.ServiceDiscovery.Telemetry
 {
     /// <summary>
-    /// <para>Creating an instance of <see cref="ServiceDiscoveryEventContextToken"/> sets a new global value of <see cref="IServiceDiscoveryEventsBuilder"/>
+    /// <para>Creating an instance of <see cref="ServiceDiscoveryEventsContextToken"/> sets a new global value of <see cref="IServiceDiscoveryEventsBuilder"/>
     /// in <see cref="FlowingContext"/>, inheriting the configuration of the previous one.</para>
     /// <para>A later <see cref="Dispose"/> call restores old value back.</para>
     /// <para>One should typically want to put created token into <c>using</c> block:</para>
@@ -19,7 +19,7 @@ namespace Vostok.ServiceDiscovery.Telemetry.EventContext
     /// </code>
     /// </summary>
     [PublicAPI]
-    public class ServiceDiscoveryEventContextToken : IDisposable
+    public class ServiceDiscoveryEventsContextToken : IDisposable
     {
         private readonly IDisposable scope;
 
@@ -28,7 +28,7 @@ namespace Vostok.ServiceDiscovery.Telemetry.EventContext
         /// inheriting the configuration of the previous one or create new if previous null.</para>
         /// <para>This constructor also captures current <see cref="IServiceDiscoveryEventsBuilder"/> which will be restored later when calling <see cref="Dispose"/>.</para>
         /// </summary>
-        public ServiceDiscoveryEventContextToken([NotNull] Action<IServiceDiscoveryEventsBuilder> setup)
+        public ServiceDiscoveryEventsContextToken([NotNull] Action<IServiceDiscoveryEventsBuilder> setup)
         {
             var newBuilder = FlowingContext.Globals.Get<ServiceDiscoveryEventsBuilder>()?.Clone() ?? new ServiceDiscoveryEventsBuilder();
             setup.Invoke(newBuilder);
@@ -39,9 +39,7 @@ namespace Vostok.ServiceDiscovery.Telemetry.EventContext
         /// <summary>
         /// Restores global <see cref="IServiceDiscoveryEventsBuilder"/> in <see cref="FlowingContext"/> to the one captured in constructor.
         /// </summary>
-        public void Dispose()
-        {
-            scope?.Dispose();
-        }
+        public void Dispose() =>
+            scope.Dispose();
     }
 }
