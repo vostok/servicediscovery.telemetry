dfcb91b40f6fd789a0a5c4d5eb56820630066c43
Ilya Shumilin <shumilin@skbkontur.ru>
2021-12-06 17:45:57 +0500

    Apply rename

---
+++ Vostok.ServiceDiscovery.Telemetry/EventsSender/DevNullServiceDiscoveryEventsSender.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using Vostok.ServiceDiscovery.Telemetry.Event;
+
+namespace Vostok.ServiceDiscovery.Telemetry.EventsSender
+{
+    /// <summary>
+    /// A trivial implementation of <see cref="IServiceDiscoveryEventsSender"/> that simply does nothing.
+    /// </summary>
+    [PublicAPI]
+    public class DevNullServiceDiscoveryEventsSender : IServiceDiscoveryEventsSender
+    {
+        public void Send(ServiceDiscoveryEvent @event)
+        {
+        }
+    }
+}
--- Vostok.ServiceDiscovery.Telemetry/EventsSender/IServiceDiscoveryEventsSender.cs
+++ Vostok.ServiceDiscovery.Telemetry/EventsSender/IServiceDiscoveryEventsSender.cs
@@ -1,14 +1,14 @@
 ﻿using JetBrains.Annotations;
 using Vostok.ServiceDiscovery.Telemetry.Event;
 
-namespace Vostok.ServiceDiscovery.Telemetry.EventSender
+namespace Vostok.ServiceDiscovery.Telemetry.EventsSender
 {
     /// <summary>
     /// Sends <see cref="ServiceDiscoveryEvent"/>s to a permanent storage.
     /// Implementations are expected to be thread-safe and exception-free.
     /// </summary>
     [PublicAPI]
-    public interface IServiceDiscoveryEventSender
+    public interface IServiceDiscoveryEventsSender
     {
         void Send([NotNull] ServiceDiscoveryEvent @event);
     }
